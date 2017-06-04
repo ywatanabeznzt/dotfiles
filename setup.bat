c3bd9b3dac585e5434d339ee2479167a20ce59c8
Yuki Watanabe <ywatanabeznzt@gmail.com>
2017-06-04 17:08:32 +0900

    Windows用のセットアップファイルを修正

--- setup.bat
+++ setup.bat
@@ -2,5 +2,6 @@ mklink %HOMEPATH%\_vimrc %HOMEPATH%\.dotfiles\vim\vimrc
 mklink %HOMEPATH%\_gvimrc %HOMEPATH%\.dotfiles\vim\rc\gui.rc.vim
 mklink %HOMEPATH%\.vim %HOMEPATH%\.dotfiles\vim
 
+cd %HOMEPATH%\.dotfiles
 git submodule init
 git submodule update
