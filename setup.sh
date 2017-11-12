4ecc9c08fcedcb5d2413c217c44f4a299d01aeb6
YukiWatanabe <ywatanabeznzt@gmail.com>
2017-11-12 19:42:21 +0900

    vimのlua supportチェック機能を追加・シンボリックシンクオプションを修正

--- setup.sh
+++ setup.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-ln -sf $HOME/.dotfiles/vim $HOME/.vim
+ln -sfn $HOME/.dotfiles/vim $HOME/.vim
 ln -sf $HOME/.dotfiles/vim/vimrc $HOME/.vimrc
 ln -sf $HOME/.dotfiles/vim/rc/gui.rc.vim $HOME/.gvimrc
 ln -sf $HOME/.dotfiles/zshrc $HOME/.zshrc
-ln -sf $HOME/.dotfiles/zplug $HOME/.zplug
+ln -sfn $HOME/.dotfiles/zplug $HOME/.zplug
 
 git submodule init
 git submodule update
+
+echo [check] vim lua support: 
+if [ -n "`vim --version | grep -o +lua`" ]; then
+    echo OK
+else
+    echo NG 2>&1
+fi
+
